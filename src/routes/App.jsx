b5f8d306f3202ae3637dd98c0c7f1e489d3493e5
JBxss <juanseb100@gmail.com>
2022-02-04 18:07:01 -0500

    Implementacion de Atomic Desing con nuevos componentes y rutas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
 import Layout from "../containers/Layout";
-import Login from "../containers/Login";
-import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PasswordRecovery from "../pages/PasswordRecovery";
+import SendEmail from "../pages/SendEmail";
+import NewPassword from "../pages/NewPassword";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
+import NotFound from "../pages/NotFound";
 import "../styles/Global.css";
 
 const App = () => {
@@ -13,12 +19,18 @@ const App = () => {
       <Layout>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />{" "}
+          <Route exact path="/login" element={<Login />} />
           <Route
             exact
             path="/recovery-password"
-            element={<RecoveryPassword />}
+            element={<PasswordRecovery />}
           />
+          <Route exact path="/send-email" element={<SendEmail />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/account" element={<MyAccount />} />
+					<Route exact path="/signup" element={<CreateAccount />} />
+					<Route exact path="/checkout" element={<Checkout />} />
+					<Route exact path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
