8cc554325d85adab5b46347d24c6a17e84c359e8
JBxss <juanseb100@gmail.com>
2022-02-19 14:39:52 -0500

    Adicion del Toggle del Menu

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "@styles/ProductItem.scss";
 
 import add from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+
+  const [cart, setCart] = useState([]);
+
+	const handleClick = () => {
+		setCart([]);
+	}
+
   return (
     <div className="ProductItem">
       <img
@@ -15,7 +22,7 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={add} alt="add" />
         </figure>
       </div>
